6afff05eb43ea02c726daff55fa297dab8213129
aledebarba <ale@alemacedo.com>
2017-11-22 22:23:21 -0200

    1st commit

---
+++ XF.Recursos/XF.Recursos.iOS/CameraFoto_iOS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using XF.Recursos.iOS;
+using XF.Recursos.API;
+using Xamarin.Media;
+
+
+[assembly: Dependency(typeof(CameraFoto_iOS))]
+namespace XF.Recursos.iOS
+{
+    public class CameraFoto_iOS : ICamera
+    {
+        public async void CapturarFoto()
+        {
+            var capturar = new MediaPicker();
+
+            MediaFile mediaPath;
+            if (capturar.IsCameraAvailable)
+            {
+                mediaPath = await capturar.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    DefaultCamera = CameraDevice.Rear,
+                    Name = string.Format("foto_{0}.jpg", DateTime.Now.ToString()),
+                    Directory = "Fiap"
+                });
+            }
+        }
+
+        public async void SelecionarFoto()
+        {
+            var capturar = new MediaPicker();
+
+            MediaFile mediaPath;
+            if (capturar.IsCameraAvailable)
+                mediaPath = await capturar.PickPhotoAsync();
+        }
+    }
+}
---
+++ XF.Recursos/XF.Recursos/Controles/EditorView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Recursos.Controles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorView : ContentPage
+    {
+        private void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Enviado!", edtDescricao.Text, "OK");
+        }
+    }
+}
